4637a0126180031f13519bb0cbb86fee20ee3f8c
Frank Mckenna <fmckenna56330@gmail.com>
2021-05-19 14:28:24 +0000

    added default cohort and asked user for hobbies ect

--- directory.rb
+++ directory.rb
@@ -1,16 +1,36 @@
 def input_students
-  puts "Please enter the name of the students"
-  puts "To finish, just hit enter twice"
+  puts "Please enter the name of the students and cohort"
+  puts "To finish, just hit enter 3 times"
   # Create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.strip
+  
+  puts "Please enter students cohort"
+  cohort = gets.strip
+  cohort = "november" if cohort.empty?
+  
+  puts "Please enter students hobbies"
+  hobbies = gets.strip
+  
+  puts "Please enter students country of birth"
+  country_of_birth = gets.strip
+  
   #while name is not empty, repeat this code.
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort.to_sym, hobbies: hobbies, country_of_birth: country_of_birth }
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = gets.strip
+    
+    if !name.empty?
+    # get another hobbies from the user
+    puts "Please enter students hobbies"
+    hobbies = gets.strip
+    # enter another coutry of birth from the user
+    puts "Please enter students country of birth"
+    country_of_birth = gets.strip
+    end
   end
   # return the array of students
   students
@@ -26,11 +46,15 @@ end
 def print(students)
   @i = 0
   filtered_students = students.reject { |some| some[:name].chr != @letter || some[:name].length >= 12 }
- 
+  
   puts "Showing all students that begin with the letter #{@letter}"
     
   until @i >= filtered_students.length
-    puts "#{@i + 1}. #{filtered_students[@i][:name]} (#{filtered_students[@i][:cohort]} cohort)"
+    student = filtered_students[@i]
+    
+  cohort = "november" if cohort.nil?
+    
+    puts "#{@i + 1}. #{student[:name]} (#{student[:cohort]} cohort, hobbies: #{student[:hobbies]}, country: #{student[:country_of_birth]})"
     @i += 1
   end
 end

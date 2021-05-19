f15864d4c910fb1eaadf18ceddd4bc10b91f0782
Frank Mckenna <fmckenna56330@gmail.com>
2021-05-19 10:38:12 +0000

    Changed print_students method to print students with specific letter

--- directory.rb
+++ directory.rb
@@ -1,34 +1,42 @@
 def input_students
-    puts "Please enter the name of the students"
-    puts "To finish, just hit enter twice"
-    # Create an empty array
-    students = []
-    # get the first name
+  puts "Please enter the name of the students"
+  puts "To finish, just hit enter twice"
+  # Create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  #while name is not empty, repeat this code.
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
     name = gets.chomp
-    #while name is not empty, repeat this code.
-    while !name.empty? do
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
-        # get another name from the user
-        name = gets.chomp
-    end
-    # return the array of students
-    students
+  end
+  # return the array of students
+  students
 end
 
 def print_header
+  puts "Enter a letter to show all students that begin with that letter"
+  @letter = gets.chomp
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  @i = 0
+  filtered_students = students.reject { |some| some[:name].chr != @letter }
+    
+  puts "Showing all students that begin with the letter #{@letter}"
+    
+  until @i >= filtered_students.length
+    puts "#{@i + 1}. #{filtered_students[@i][:name]} (#{filtered_students[@i][:cohort]} cohort)"
+    @i += 1
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{@i} great students"
 end
 #nothing happes until we call the methods
 students = input_students
